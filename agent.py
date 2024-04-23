ad3a93882a244a3b251e6eda8aae1afef350b253
crystal / ue <mfihy1@gmail.com>
2024-04-23 18:57:45 -0400

    Add plot helper

--- agent.py
+++ agent.py
@@ -4,6 +4,7 @@ import numpy as np
 from collections import deque
 from game import SnakeGameAI, Direction, Point, BLOCK_SIZE
 from model import Linear_QNet, QTrainer
+from plot import plot
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
@@ -128,7 +129,13 @@ def train():
                 agent.model.save()
             
             print("Game", agent.n_games, "Score", score, "Record:", record)
-            # plot
+            
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            
+            plot(plot_scores, plot_mean_scores)
 
 if __name__ == '__main__':
     train()

15a50e88316a3a391081220c00fc66ceca4335d3
crystal / ue <mfihy1@gmail.com>
2024-04-23 16:31:27 -0400

    Add agent.py

---
+++ agent.py
@@ -0,0 +1,28 @@
+import torch
+import random
+import numpy as np
+from collections import deque
+from game import SnakeGameAI, Direction, Point
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+
+class Agent:
+    def __init__(self):
+        pass
+
+    def get_state(self, game):
+        pass
+
+    def remember(self, action, reward, next_state, game_over):
+        pass
+    
+    def train_long_memory(self):
+        pass
+
+    def train_short_memory(self):
+        pass
+
+    def get_action(self, state):
+        pass

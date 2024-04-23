a29cc6e5d893c4c1bfaed94e64c6ba0940dca5b3
crystal / ue <mfihy1@gmail.com>
2024-04-23 18:24:57 -0400

    Implement get action and memory functions

--- agent.py
+++ agent.py
@@ -14,6 +14,8 @@ class Agent:
         self.epsilon = 0 # Control the randomness
         self.gamma = 0 # Discount rate
         self.memory = deque(maxlen=MAX_MEMORY)
+        self.model = None
+        self.trainer = None
         
     def get_state(self, game):
         head = game.snake[0]
@@ -58,17 +60,37 @@ class Agent:
         
         return np.array(state, dtype=int)
 
-    def remember(self, action, reward, next_state, game_over):
-        pass
+    def remember(self, state, action, reward, next_state, game_over):
+        self.memory.append((state, action, reward, next_state, game_over))
     
     def train_long_memory(self):
-        pass
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE)
+        else:
+            mini_sample = self.memory
 
-    def train_short_memory(self, action, reward, next_state, game_over):
-        pass
+        states, actions, rewards, next_states, game_overs = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, game_overs)
+
+    def train_short_memory(self, state, action, reward, next_state, game_over):
+        self.trainer.train_step(state, action, reward, next_state, game_over)
 
     def get_action(self, state):
-        pass
+        # Random moves: tradeoff exploration/exploitation 
+        self.epsilon = 80 - self.n_games
+        final_move = [0,0,0]
+        
+        if random.randint(0, 200) < self.epsilon:
+            move = random.randint(0, 2)
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model.predict(state0)
+            move = max(prediction).item()
+        
+        final_move[move] = 1
+
+        return final_move
+
 
 def train():
     plot_scores = []
